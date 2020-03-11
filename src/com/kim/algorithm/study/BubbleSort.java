816a8e3d892e6c36b1835437aae969c0d4a61600
kim <sysout0418@gmail.com>
2020-03-11 16:52:17 +0900

    2020.03.11 버블정렬 알고리즘 내용 수정

--- src/com/kim/algorithm/study/BubbleSort.java
+++ src/com/kim/algorithm/study/BubbleSort.java
@@ -2,6 +2,12 @@ package com.kim.algorithm.study;
 
 import java.util.Arrays;
 
+/**
+ * 버블 정렬 알고리즘
+ *
+ * 최선의 성능 O(n) : 리스트가 이미 정렬되어 있을 때
+ * 최악의 성능 O(n^2) : 리스트가 역순으로 정렬되어 있을 때
+ */
 public class BubbleSort {
     public static void main(String[] args) {
         int[] numbers = {4, 7, 1, 6, 3, 5, 4};
